6fd7f978306146c4f328c4cb31ab1c74749f5bb3
mploed <michael.ploed@gmail.com>
2016-08-05 07:07:21 +0200

    Spring Boot 1.4

---
+++ src/test/java/com/innoq/schulung/spring/demo/test/SpringBootTest.java
@@ -0,0 +1,13 @@
+package com.innoq.schulung.spring.demo.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@org.springframework.boot.test.context.SpringBootTest
+public class SpringBootTest {
+    @Test
+    public void contextLoads() {
+    }
+}
